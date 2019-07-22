bee4b94c0368cebce717f860e61103dfba84f06f
fajaru <fajarudewangga@gmail.com>
2019-07-22 19:22:08 +0800

    passing method between components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import Students from './Students/Students';
+import React, { useState } from "react";
+import "./App.css";
+import Students from "./Students/Students";
 
-class App extends Component {
-  state = {
-    name: [
-      { name: "Fajaru", age: 20 },
-      { name: "Fajar", age: 25 }
-    ],
-    spirit: "KEEP THE SPIRIT UP DUDE, YOU CAN DO IT"
-  }
+const App = props => {
+  const [studentsState, setStudentsState] = useState({
+    name: [{ name: "Fajaru", age: 20 }, { name: "Fajar", age: 25 }]
+  });
 
-  switchNameHandler = () => {
+  const [spiritState, setSpiritState] = useState("KEEP THE SPIRIT UP DUDE");
+
+  console.log(studentsState);
+
+  const switchNameHandler = () => {
     // alert("HEHE");
     // this.state.name[0].name = "NEW FAJARU"; DON'T DO THIS STUPID SYNTAX
-    this.setState({
-      name: [
-        { name: "Bhirawa", age: 30 },
-        { name: "Dewangga", age: 35 }
-      ]
-    })
-  }
+    setStudentsState({
+      name: [{ name: "Bhirawa", age: 30 }, { name: "Dewangga", age: 35 }]
+    });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, Let's Make React App</h1>
-        <p>This is Working!</p>
-        <button onClick={this.switchNameHandler}>Switch Names</button>
-        {this.state.name.map((e) =>
-          <Students name={e.name} age={e.age}/>
-        )}
-        <h2>{this.state.spirit}</h2>
-      </div>
-      // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, Let\'s Make React App'));
-    );
-  }
-}
+  const switchSpiritHandler = newMessage => {
+    setSpiritState(newMessage);
+  };
+
+  const switchHandler = newMessage => {
+    switchNameHandler();
+    switchSpiritHandler(newMessage);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, Let's Make React App</h1>
+      <p>This is Working!</p>
+      {/* <button onClick={() => switchHandler("YOU KNOW YOU CAN DO IT")}>
+        Switch Names
+      </button> */}
+      <button onClick={switchHandler.bind(this, "YOU KNOW YOU CAN DO IT")}>
+        Switch Names
+      </button>
+      {studentsState.name.map(e => (
+        <Students name={e.name} age={e.age} />
+      ))}
+      <h2>{spiritState}</h2>
+    </div>
+  );
+  // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, Let\'s Make React App'));
+};
 
 export default App;
--- src/Students/Students.js
+++ src/Students/Students.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const student = (props) => {
+const student = props => {
   return (
     <div>
-      <p>This is The React App That {props.name} Made, and He's {props.age} years old</p>
+      <p>
+        This is The React App That {props.name} Made, and He's {props.age} years
+        old
+      </p>
       <p>{props.children}</p>
     </div>
-  )
-}
+  );
+};
 
 export default student;
