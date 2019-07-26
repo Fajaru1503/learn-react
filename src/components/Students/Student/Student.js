2aeccc5c7574d6c02238e6fbb5733ef957b0c60d
fajaru <fajarudewangga@gmail.com>
2019-07-26 13:49:13 +0800

    get better structure folder

--- src/components/Students/Student/Student.js
+++ src/components/Students/Student/Student.js
@@ -1,9 +1,9 @@
 import React from "react";
-import studentClasses from "./Students.css";
+import studentClasses from "./Student.css";
 
 const student = props => {
   return (
-    <div className={studentClasses.Students}>
+    <div className={studentClasses.Student}>
       <p onClick={props.deleteData}>
         This is The React App That {props.name} Made, and He's {props.age} years
         old
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,7 @@
-import React, { useState, Component } from "react";
+import React, { Component } from "react";
 import appClasses from "./App.css";
-import Students from "./Students/Students";
+import Student from "../components/Students/Student/Student";
 
-// function base
-// const App = props => {
-//   const [studentsState, setStudentsState] = useState({
-//     name: [{ name: "Fajaru", age: 20 }, { name: "Fajar", age: 25 }]
-//   });
-
-//   const [spiritState, setSpiritState] = useState("KEEP THE SPIRIT UP DUDE");
-
-//   console.log(studentsState);
-
-//   const switchNameHandler = newNames => {
-//     // this.state.name[0].name = "NEW FAJARU"; DON'T DO THIS STUPID SYNTAX
-//     setStudentsState({
-//       name: [{ name: "Bhirawa", age: 30 }, { name: "Dewangga", age: 35 }]
-//     });
-//   };
-
-//   const changNamesHandler = e => {
-//     setStudentsState({
-//       name: [{ name: e.target.value, age: 20 }, { name: "Fajar", age: 25 }]
-//     });
-//   };
-
-//   const switchSpiritHandler = newMessage => {
-//     setSpiritState(newMessage);
-//   };
-
-//   const switchHandler = newMessage => {
-//     switchNameHandler();
-//     switchSpiritHandler(newMessage);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, Let's Make React App</h1>
-//       <p>This is Working!</p>
-//       {/* <button onClick={() => switchHandler("YOU KNOW YOU CAN DO IT")}>Switch Names</button> NOT RECOMENDED */}
-//       <button onClick={switchHandler.bind(this, "YOU KNOW YOU CAN DO IT")}>
-//         Switch Names
-//       </button>
-//       {studentsState.name.map(e => (
-//         <Students name={e.name} age={e.age} setNames={changNamesHandler} />
-//       ))}
-//       <h2>{spiritState}</h2>
-//     </div>
-//   );
-//   // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, Let\'s Make React App'));
-// };
-
-// class based
 class App extends Component {
   state = {
     students: [
@@ -105,7 +55,7 @@ class App extends Component {
       datas = (
         <div>
           {this.state.students.map((student, index) => (
-            <Students
+            <Student
               name={student.name}
               age={student.age}
               key={student.id}
